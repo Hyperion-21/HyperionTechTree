2bc60d4a07180a43508c2586cb59072dffc597a7
Hyperion21 <Hyperion21.personal@gmail.com>
2023-04-01 00:21:24 -0700

    Broken, throws frustrating error

---
+++ HyperionTechTreeProject/HyperionJsonReader.cs
@@ -0,0 +1,65 @@
+
+
+using BepInEx;
+using BepInEx.Logging;
+using Newtonsoft.Json;
+
+namespace HyperionTechTree;
+
+public class HyperionJsonReader
+{
+    private string _jsonFilePath;
+    //private string _jsonString;
+    private JsonTextReader _reader;
+    private static ManualLogSource _logger;
+
+    /// <summary>
+    /// A json reader class made by Hyperion_21. Construct using a file path to the target .json file.
+    /// </summary>
+    /// <param name="jsonFilePath"></param>
+    public HyperionJsonReader(string jsonFilePath)
+    {
+        this._jsonFilePath = jsonFilePath;
+        //this._jsonString = File.ReadAllText(jsonFilePath);
+        _reader = new(new StringReader(File.ReadAllText(jsonFilePath)));
+        _logger = HyperionTechTreePlugin.HyperionLog;
+
+        _logger.LogInfo($"jsonString: {File.ReadAllText(jsonFilePath)}");
+    }
+
+    /// <summary>
+    /// Inputs a key to look for in the .json, and returns that key's value. Return type depends on the key, so explicit casts may be needed!
+    /// </summary>
+    /// <param name="key"></param>
+    /// <returns name="value"></returns>
+    public object GetValueFromKey(string key)
+    {
+        if (string.IsNullOrEmpty(key))
+        {
+            _logger.LogError("Could not run GetValueFromKey! Returning null.");
+            return null;
+        }
+        _logger.LogInfo($"GetValueFromKey run for key {key}.");
+        while (_reader.Read())
+        {
+            if (_reader.Value == null) continue;
+            if (_reader.Value.ToString() == key)
+            {
+                break;
+            }
+        }
+        _reader.Read();
+        var value = _reader.Value;
+        _logger.LogInfo($"Found value {value}!");
+        ResetJsonReader();
+        return value;
+    }
+
+    public void ResetJsonReader()
+    {
+        _logger.LogInfo("Attempting to reset json reader!");
+        _reader.Close();
+        //_reader = null;
+        _reader = new(new StringReader(_jsonFilePath));
+    } 
+}
--- HyperionTechTreeProject/HyperionTechTreePlugin.cs
+++ HyperionTechTreeProject/HyperionTechTreePlugin.cs
@@ -9,6 +9,9 @@ using SpaceWarp.API.Game.Extensions;
 using SpaceWarp.API.UI;
 using SpaceWarp.API.UI.Appbar;
 using UnityEngine;
+using BepInEx.Logging;
+using System.Runtime.InteropServices.ComTypes;
+using Newtonsoft.Json;
 
 namespace HyperionTechTree;
 
@@ -16,6 +19,11 @@ namespace HyperionTechTree;
 [BepInDependency(SpaceWarpPlugin.ModGuid, SpaceWarpPlugin.ModVer)]
 public class HyperionTechTreePlugin : BaseSpaceWarpPlugin
 {
+    // REMEMBER! 
+    // Use _camelCase for private variables
+    // Use camelCase for local variables (defined in method)
+    // Use PascalCase for just about everything else
+
     // These are useful in case some other mod wants to add a dependency to this one
     public const string ModGuid = MyPluginInfo.PLUGIN_GUID;
     public const string ModName = MyPluginInfo.PLUGIN_NAME;
@@ -27,6 +35,15 @@ public class HyperionTechTreePlugin : BaseSpaceWarpPlugin
     private const string ToolbarFlightButtonID = "BTN-HyperionTechTreeFlight";
     private const string ToolbarOABButtonID = "BTN-HyperionTechTreeOAB";
 
+    private static HyperionJsonReader _reader;
+    private string DefaultTechTreeFilePath => $"{PluginFolderPath}/Tech Tree/TechTree.json";
+
+    // idk how loggers work. HyperionLog is designed for other classes to use it without needing BepInEx shenanigans in that class. Recommended to use _logger if working with this project.
+    internal static ManualLogSource HyperionLog;
+    private static ManualLogSource _logger;
+
+    private static JsonTextReader _testreader;
+
     public static HyperionTechTreePlugin Instance { get; set; }
 
     /// <summary>
@@ -35,6 +52,9 @@ public class HyperionTechTreePlugin : BaseSpaceWarpPlugin
     public override void OnInitialized()
     {
         base.OnInitialized();
+
+        HyperionLog = Logger;
+        _logger = HyperionLog;
 
         Instance = this;
 
@@ -65,24 +85,34 @@ public class HyperionTechTreePlugin : BaseSpaceWarpPlugin
         // Register all Harmony patches in the project
         Harmony.CreateAndPatchAll(typeof(HyperionTechTreePlugin).Assembly);
 
-        // Try to get the currently active vessel, set its throttle to 100% and toggle on the landing gear
-        try
-        {
-            var currentVessel = Vehicle.ActiveVesselVehicle;
-            if (currentVessel != null)
-            {
-                currentVessel.SetMainThrottle(1.0f);
-                currentVessel.SetGearState(true);
-            }
-        }
-        catch (Exception e) {}
-        
+        //// Try to get the currently active vessel, set its throttle to 100% and toggle on the landing gear
+        //// (might use part of this code later for science point awarding)
+        //try
+        //{
+        //    var currentVessel = Vehicle.ActiveVesselVehicle;
+        //    if (currentVessel != null)
+        //    {
+        //        currentVessel.SetMainThrottle(1.0f);
+        //        currentVessel.SetGearState(true);
+        //    }
+        //}
+        //
+        //catch (Exception e) {}
+
         // Fetch a configuration value or create a default one if it does not exist
-        var defaultValue = "my_value";
-        var configValue = Config.Bind<string>("Settings section", "Option 1", defaultValue, "Option description");
-        
-        // Log the config value into <KSP2 Root>/BepInEx/LogOutput.log
-        Logger.LogInfo($"Option 1: {configValue.Value}");
+        //var defaultValue = "my_value";
+        //var configValue = Config.Bind<string>("Settings section", "Option 1", defaultValue, "Option description");
+
+        //// Log the config value into <KSP2 Root>/BepInEx/LogOutput.log
+        //Logger.LogInfo($"Option 1: {configValue.Value}");
+
+        // Change this later to support multiple files
+        _testreader = new(new StringReader(File.ReadAllText(DefaultTechTreeFilePath)));
+
+        _logger.LogInfo($"Creating json reader from {DefaultTechTreeFilePath}");
+        if (File.Exists(DefaultTechTreeFilePath)) _reader = new(DefaultTechTreeFilePath);
+        else _logger.LogInfo($"Could not find file at {DefaultTechTreeFilePath}!");
+
     }
 
     /// <summary>
@@ -112,7 +142,18 @@ public class HyperionTechTreePlugin : BaseSpaceWarpPlugin
     /// <param name="windowID"></param>
     private static void FillWindow(int windowID)
     {
-        GUILayout.Label("Hyperion Tech Tree - A modded tech tree independent of the stock KSP2 tech system.");
+        _testreader.Read();
+
+        string nodeID = (string)_reader.GetValueFromKey("nodeID");
+        _logger.LogInfo(nodeID);
+        
+        //string[] dependencies = (string[])_reader.GetValueFromKey("dependencies");
+        //string[] parts = (string[])_reader.GetValueFromKey("parts");
+
+        GUILayout.Label($"First Node ID: {nodeID}");
+        //GUILayout.Label(dependencies.Length > 0 ? $"First Node First Dependency: {dependencies[0]}" : "There are no dependencies for this node!");
+        //GUILayout.Label(parts.Length > 0 ? $"First Node First Part: {parts[0]}" : "There are no parts for this node!");
+
         GUI.DragWindow(new Rect(0, 0, 10000, 500));
     }
 }
